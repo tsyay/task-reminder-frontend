0031681555aad8a787f5eddb78b66c357f58ddaa
tsyay <tsyay.workmail@gmail.com>
2025-11-29 12:47:04 +0800

    style: improve reminders table layout and reminders page

--- src/app/pages/reminders-page/reminders-page.component.ts
+++ src/app/pages/reminders-page/reminders-page.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { RemindersTableComponent } from '../../features/reminder/list-reminders/ui/reminders-table.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { RouterModule } from '@angular/router';
 
 @Component({
-    standalone: true,
-      selector: 'app-reminders-page',
-      templateUrl: './reminders-page.component.html',
-      imports: [RemindersTableComponent],
+  standalone: true,
+  selector: 'app-reminders-page',
+  templateUrl: './reminders-page.component.html',
+  styleUrl: './reminders-page.component.less',
+  imports: [
+    RemindersTableComponent,
+    MatButtonModule,
+    RouterModule,
+    MatDividerModule,
+  ],
 })
-
 export class RemindersPageComponent {}
